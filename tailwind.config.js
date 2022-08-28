147402312d8bcd3f9e05ba20d1759a7c080aa4cf
haalvberg <hakon.helberg@gmail.com>
2022-08-28 15:34:57 +0200

    Fixed content configuration and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
     fontFamily: {
       sans: ['Sora', 'sans-serif'],
       serif: ['Just Another Hand', 'serif'],
+    },
+    container: {
+      center: true,
+      padding: '6rem',
     },
     extend: {
       colors: {
